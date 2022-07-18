caf37f5444b3035cd8c0b37f69dceb528bca2005
김민석 <95396532+biber5515@users.noreply.github.com>
2022-07-18 23:34:53 +0900

    비밀번호 설정

--- app/src/main/java/com/example/secretdiary/MainActivity.kt
+++ app/src/main/java/com/example/secretdiary/MainActivity.kt
@@ -1,11 +1,113 @@
 package com.example.secretdiary
 
+import android.content.Context
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.NumberPicker
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.widget.AppCompatButton
+import androidx.core.content.edit
 
 class MainActivity : AppCompatActivity() {
+
+    private val numberPicker1: NumberPicker by lazy {
+        findViewById<NumberPicker>(R.id.numberpicke1)
+            .apply {
+                minValue=0
+                maxValue=9
+            }
+    }
+
+    private val numberPicker2: NumberPicker by lazy {
+        findViewById<NumberPicker>(R.id.numberpicke2)
+            .apply {
+                minValue=0
+                maxValue=9
+            }
+
+    }
+
+    private val numberPicker3: NumberPicker by lazy {
+        findViewById<NumberPicker>(R.id.numberpicke3)
+            .apply {
+                minValue=0
+                maxValue=9
+            }
+    }
+
+    private val openButton:AppCompatButton by lazy {
+        findViewById<AppCompatButton>(R.id.openButton)
+    }
+
+    private val changePasswordButton : AppCompatButton by lazy{
+        findViewById<AppCompatButton>(R.id.changePasswordButton)
+    }
+
+    private var changePassWordMode=false
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        numberPicker1
+        numberPicker2
+        numberPicker3
+
+        openButton.setOnClickListener {
+
+            if(changePassWordMode){
+                Toast.makeText(this,"비밀번호 변경중입니다",Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            val passwordPreferences=getSharedPreferences("password", Context.MODE_PRIVATE)
+            val passwordFromUser = "${numberPicker1.value}${numberPicker2.value}${numberPicker3.value}"
+
+           if(passwordPreferences.getString("password","000").equals(passwordFromUser)){
+               //패스워드 성공
+
+               //TODO 다이어리 페이지 작성후에 넘겨주어야함
+               //startActivity()
+           }else{
+               showErrorAlertDialog()
+            }
+        }
+        changePasswordButton.setOnClickListener {
+            val passwordPreferences=getSharedPreferences("password", Context.MODE_PRIVATE)
+            val passwordFromUser = "${numberPicker1.value}${numberPicker2.value}${numberPicker3.value}"
+            if(changePassWordMode){
+
+
+                passwordPreferences.edit(true){
+                    putString("password",passwordFromUser)
+                }
+                changePassWordMode=false
+                changePasswordButton.setBackgroundColor(Color.BLACK)
+            }else{
+
+                if(passwordPreferences.getString("password","000").equals(passwordFromUser)){
+                    changePassWordMode=true
+                    Toast.makeText(this,"변경할 패스워드를 입력해주세요",Toast.LENGTH_SHORT).show()
+                    changePasswordButton.setBackgroundColor(Color.RED)
+                }else{
+                    showErrorAlertDialog()
+                }
+
+            }
+        }
+
     }
+    private fun showErrorAlertDialog(){
+        AlertDialog.Builder(this)
+            .setTitle("실패!!")
+            .setMessage("비밀번호가 잘못되었습니다!!")
+            .setPositiveButton("확인"){ _, _ -> }
+            .create()
+            .show()
+
+    }
+
 }
